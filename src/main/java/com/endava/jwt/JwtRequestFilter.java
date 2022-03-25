735e0db68f2cf4b00b78d5dbb189b24ae3bdc707
danish.shakil <danishshakil15@yahoo.com>
2022-03-24 22:09:45 -0400

    removed comment

--- src/main/java/com/endava/jwt/JwtRequestFilter.java
+++ src/main/java/com/endava/jwt/JwtRequestFilter.java
@@ -31,7 +31,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String auth = request.getHeader("Authorization");
         String token = null, userName = null;
 
-        if (auth != null && auth.startsWith("Bearer")){ //test
+        if (auth != null && auth.startsWith("Bearer")){
             token = auth.substring(7);
             userName = jwtUtil.getUsernameFromToken(token);
         }
